bcb1329cad64a8f051cfcd3a38f214350ea555d1
BonagucciAI202 <78320452+imgod123456@users.noreply.github.com>
2021-07-15 06:36:23 +0000

    Initialize 6.0.x

---
+++ core/cas-server-core-tickets/src/test/java/org/apereo/cas/ticket/InvalidTicketExceptionTests.java
@@ -0,0 +1,19 @@
+package org.apereo.cas.ticket;
+
+import lombok.val;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Misagh Moayyed
+ * @since 3.0.0
+ */
+public class InvalidTicketExceptionTests {
+
+    @Test
+    public void verifyCodeNoThrowable() {
+        val t = new InvalidTicketException("InvalidTicketId");
+        assertEquals("INVALID_TICKET", t.getCode());
+    }
+}
